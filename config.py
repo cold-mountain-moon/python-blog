3b6376f7cd58588f28885e49abfca758073f69ca
cold-mountain-moon <2439730644@qq.com>
2018-09-07 18:02:40 +0800

    config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 # @Author: anchen
 # @Date:   2018-08-31 16:25:49
 # @Last Modified by:   anchen
-# @Last Modified time: 2018-08-31 16:31:53
+# @Last Modified time: 2018-09-03 11:08:01
 import os
 
 SECRET_KEY = os.urandom(24)
@@ -16,7 +16,9 @@ USERNAME='root'
 PASSWORD='root'
 HOST='127.0.0.1'
 PORT='3306'
-DATABASE='blog-db'
+DATABASE='blog_db'
 
 SQLALCHEMY_DATABASE_URI='{}+{}://{}:{}@{}:{}/{}?charset=utf8'.format(DIALECT, DRIVER, USERNAME, PASSWORD, HOST, PORT, DATABASE)
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
