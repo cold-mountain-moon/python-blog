112f5387c512e0f90a34ffcc3fb657e4284a5e03
cold-mountain-moon <2439730644@qq.com>
2018-09-13 19:06:09 +0800

    留言板页面、文章页面、markdown解析支持、照片墙瀑布流支持

--- models.py
+++ models.py
@@ -3,7 +3,7 @@
 # @Author: anchen
 # @Date:   2018-08-31 16:36:57
 # @Last Modified by:   anchen
-# @Last Modified time: 2018-09-07 15:06:09
+# @Last Modified time: 2018-09-12 13:58:11
 
 import datetime
 from exts import db
@@ -22,13 +22,18 @@ class User(db.Model):
     portrait = db.Column(db.LargeBinary)
     job = db.Column(db.String(32), nullable = False)
     descr = db.Column(db.Text, nullable = False)
-    createdate = db.Column(db.Date, nullable = False)
-    updatedate = db.Column(db.Date)
+    createdate = db.Column(db.DateTime, nullable = False)
+    updatedate = db.Column(db.DateTime, nullable = False)
 
-    def __init__(self, loginname, name,  telephone, password):
+    def __init__(self, loginname, name,  telephone, password, sex, interests, job, descr):
         self.createdate = datetime.datetime.now()
+        self.updatedate = datetime.datetime.now()
         self.loginname = loginname
         self.name = name
+        self.sex = sex
+        self.interests = interests
+        self.job = job
+        self.descr = descr
         self.telephone = telephone
         self.password = password
 
@@ -37,19 +42,20 @@ class Catogory(db.Model):
     __tablename__ = 'catogory'
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     name = db.Column(db.String(64), nullable = False, unique = True)
-    createdate = db.Column(db.Date, nullable = False)
-    updatedate = db.Column(db.Date)
+    createdate = db.Column(db.DateTime, nullable = False)
+    updatedate = db.Column(db.DateTime)
 
     def __init__(self, name):
         self.createdate = datetime.datetime.now()
+        self.updatedate = datetime.datetime.now()
         self.name = name
 
 class Tag(db.Model):
     __tablename__ = 'tag'
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     name = db.Column(db.String(32), nullable = False, unique = True)
-    createdate = db.Column(db.Date, nullable = False)
-    updatedate = db.Column(db.Date)
+    createdate = db.Column(db.DateTime, nullable = False)
+    updatedate = db.Column(db.DateTime, nullable = False)
 
 
 class Article(db.Model):
@@ -59,11 +65,11 @@ class Article(db.Model):
     authorid = db.Column(db.Integer, db.ForeignKey('user.id'))
     catogoryid = db.Column(db.Integer, db.ForeignKey('catogory.id'))
     content = db.Column(db.Text, nullable = False)
-    createdate = db.Column(db.Date, nullable = False)
-    updatedate = db.Column(db.Date)
+    createdate = db.Column(db.DateTime, nullable = False)
+    updatedate = db.Column(db.DateTime, nullable = False)
 
     author = db.relationship('User', backref=db.backref('articles'))
-    catogory = db.relationship('Catogory', backref=db.backref('catogories'))
+    catogory = db.relationship('Catogory', backref=db.backref('articles'))
 
     def __init__(self, title, authorid, catogoryid, content):
         self.title = title
@@ -71,5 +77,56 @@ class Article(db.Model):
         self.catogoryid = catogoryid
         self.content = content
         self.createdate = datetime.datetime.now()
+        self.updatedate = datetime.datetime.now()
+
+class Album(db.Model):
+    __tablename__ = 'album'
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    name = db.Column(db.String(64), nullable = False)
+    descr = db.Column(db.String(255), nullable = False)
+    cover = db.Column(db.String(512), nullable = False)
+    userid = db.Column(db.Integer, db.ForeignKey('user.id'))
+    createdate = db.Column(db.DateTime, nullable = False)
+    updatedate = db.Column(db.DateTime, nullable = False)
+
+    user = db.relationship('User', backref=db.backref('albums'))
+
+    def __init__(self, name, descr, cover):
+        self.name = name
+        self.descr = descr
+        self.cover = cover
+        self.createdate = datetime.datetime.now()
+        self.updatedate = datetime.datetime.now()
+
+class Photo(db.Model):
+    __tablename__ = 'photo'
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    path = db.Column(db.String(512), nullable = False)
+    albumid = db.Column(db.Integer, db.ForeignKey('album.id'))
+    createdate = db.Column(db.DateTime, nullable = False)
+    updatedate = db.Column(db.DateTime, nullable = False)
+
+    album = db.relationship('Album', backref=db.backref('photos'))
+
+    def __init__(self, path, albumid):
+        self.path = path
+        self.albumid = albumid
+        self.createdate = datetime.datetime.now()
+        self.updatedate = datetime.datetime.now()
+
+class Emotion(db.Model):
+    __tablename__ = 'emotion'
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    content = db.Column(db.String(1024), nullable = False)
+    userid = db.Column(db.Integer, db.ForeignKey('user.id'))
+    createdate = db.Column(db.DateTime, nullable = False)
+    updatedate = db.Column(db.DateTime, nullable = False)
+
+    user = db.relationship('User', backref = db.backref('emotions'))
+
+    def __init__(self, content):
+        self.content = content
+        self.createdate = datetime.datetime.now()
+        self.updatedate = datetime.datetime.now()
 
 
--- python_blog.py
+++ python_blog.py
@@ -3,11 +3,11 @@
 # @Author: anchen
 # @Date:   2018-08-31 16:25:39
 # @Last Modified by:   anchen
-# @Last Modified time: 2018-09-04 16:09:06
-from flask import Flask, render_template, request, redirect, session, url_for, jsonify
+# @Last Modified time: 2018-09-13 16:39:04
+from flask import Flask, render_template, request, redirect, session, url_for, jsonify, g
 from exts import db
 import config
-from models import User, Article
+from models import User, Article,Catogory, Album, Photo, Tag
 
 app = Flask(__name__)
 app.config.from_object(config)
@@ -16,7 +16,8 @@ db.init_app(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    
+    return render_template('index.html', main_catogories=g.main_catogories)
 
 @app.route('/regist/', methods=['GET', 'POST'])
 def regist():
@@ -64,36 +65,48 @@ def logout():
     session.clear()
     return redirect(url_for('login'))
 
-@app.route('/article/')
-def article_list():
-    articles = Article.query.all()
+@app.route('/catogory/article/<catogoryid>')
+def article_list(catogoryid):
+
+    articles = Article.query.filter(Article.catogoryid==catogoryid)
     return render_template('article/list.html', articles=articles)
 
 @app.route('/article/<id>', methods=['GET'])
 def article_detail(id):
     article = Article.query.filter(Article.id == id).first()
-    content = '<p class="small-content">' + article.content + '</p>'
-    if content:
-        split = content.split(u'\n\r\t')
-        print(len(split))
-
-        for s in split:
-            s += u'</p><p class="small-content">'
-        content = s
-        print(content)
-    if not article:
-        article = {}
+    print('===================55555555555')
     return render_template('article/detail.html', article=article)
-   # return jsonify({'id':article.id, 'content': article.content, 'authorid': article.authorid})
+
+@app.route('/album/', methods = ['GET'])
+def album_list():
+    albums = Album.query.all()
+    return render_template('album/list.html', albums = albums)
+
+@app.route('/album/photo/<id>')
+def album_photo_list(id):
+    photos = Photo.query.filter(Photo.albumid == id)
+    return render_template('photo/list.html', photos = photos)
+
+@app.route('/message/board/')
+def message_board_list():
+    return render_template('messageboard/list.html')
 
 @app.context_processor
-def get_current_user():
+def every_request():
+    result = {'main_catogories': g.main_catogories}
     user_id = session.get('user_id')
+
     if user_id:
         user = User.query.filter(User.id == user_id).first()
-        return {'user': user}
-    else:
-        return {}
+        result['user'] = user
+    return result
+
+@app.before_request
+def first_request():
+    if not hasattr(g, 'main_catogories'):
+        catogories = Catogory.query.all()
+        catogories = filter(lambda catogory: len(catogory.articles) > 0, catogories)
+        setattr(g, 'main_catogories', catogories)
 
 
 
